8a453d87b3c1cf5ee42383418a8702c42578811f
tefi-rivera <estephany18rivera@gmail.com>
2018-12-06 18:40:51 -0600

    Nueva Actualizacion

---
+++ app/src/main/java/com/example/dherrera/recyclerview00/Adaptador.java
@@ -0,0 +1,41 @@
+package com.example.dherrera.recyclerview00;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by dherrera on 15/03/2017.
+ */
+
+public class Adaptador extends RecyclerView.Adapter<viewHolder> {
+
+    List<Fuente> ListaObjeto;
+
+
+    public Adaptador(List<Fuente> listaObjeto) {
+        ListaObjeto = listaObjeto;
+    }
+
+    @Override
+    public viewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        return new viewHolder(vista,ListaObjeto);
+    }
+
+    @Override
+    public void onBindViewHolder(viewHolder holder, int position) {
+         holder.titulo.setText(ListaObjeto.get(position).getTitulo());
+         holder.imagen.setImageResource(ListaObjeto.get(position).getImagen());
+    }
+
+    @Override
+    public int getItemCount() {
+        return ListaObjeto.size();
+    }
+
+
+}
